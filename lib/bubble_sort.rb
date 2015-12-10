22341e1309eddbbf77eef4de3b3ef581879a2b0f
Erinna Chen <erinna.chen@gmail.com>
2015-12-10 15:59:41 -0700

    Working bubblesort with tests

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,17 +1,23 @@
 class BubbleSort
 
   def sort(unsorted)
-    if unsorted.length > 1
-      unsorted = swap(unsorted[0],unsorted[1]) if unsorted[0] > unsorted[1]
+    length = unsorted.length
+    if length > 1
+      length.times do
+        (0...length-1).each do |i|
+          a,b = unsorted[i,2]
+          unsorted[i], unsorted[i+1] = swap(a,b) if wrong_order?(a,b)
+        end
+      end
     end
-    unsorted
+    sorted = unsorted
   end
 
   def swap(a,b)
     [b, a]
   end
 
+  def wrong_order?(a,b)
+    a > b
+  end
 end
-
-bs = BubbleSort.new
-puts bs.sort("ba".chars).inspect
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -23,6 +23,19 @@ class BubbleSortTest < Minitest::Test
     assert_equal ["a", "b"], bs.sort("ba".chars)
   end
 
+  def test_sample_sort
+    bs = BubbleSort.new
+    to_sort = [4,2,0,3,1]
+    assert_equal [0,1,2,3,4], bs.sort(to_sort)
+  end
 
+  def test_sort_a_bunch_of_elements
+    bs = BubbleSort.new
+    to_sort = (1..100).to_a
+    unsorted = to_sort.shuffle
+    unsorted = to_sort.shuffle while unsorted == to_sort
+    assert_equal to_sort, bs.sort(unsorted)
+
+  end
 
 end
