2354e815a8d3d47da4e91ec1ecfc6f9f4cade99d
Erinna Chen <erinna.chen@gmail.com>
2015-12-10 15:28:23 -0700

    Bubblesort works for two elements

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,7 +1,17 @@
 class BubbleSort
 
   def sort(unsorted)
+    if unsorted.length > 1
+      unsorted = swap(unsorted[0],unsorted[1]) if unsorted[0] > unsorted[1]
+    end
     unsorted
   end
 
+  def swap(a,b)
+    [b, a]
+  end
+
 end
+
+bs = BubbleSort.new
+puts bs.sort("ba".chars).inspect
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -12,4 +12,17 @@ class BubbleSortTest < Minitest::Test
     assert_equal ["a"], bs.sort(["a"])
   end
 
+  def test_swaps_two_elements_correctly
+    bs = BubbleSort.new
+    assert_equal ["b", "a"],bs.swap("a","b")
+  end
+
+  def test_returns_two_elements_sorted_correctly
+    bs = BubbleSort.new
+    assert_equal ["a", "b"], bs.sort("ab".chars)
+    assert_equal ["a", "b"], bs.sort("ba".chars)
+  end
+
+
+
 end
