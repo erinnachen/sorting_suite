d87e69600d51645815981a54f70aaa7cf8fa7f8c
Erinna Chen <erinna.chen@gmail.com>
2015-12-10 18:37:51 -0700

    Refactored bubblesort

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,13 +1,10 @@
 class BubbleSort
-
   def sort(unsorted)
     length = unsorted.length
-    if length > 1
-      length.times do
-        (0...length-1).each do |i|
-          a,b = unsorted[i,2]
-          unsorted[i], unsorted[i+1] = swap(a,b) if wrong_order?(a,b)
-        end
+    length.times do
+      (0...length-1).each do |i|
+        a,b = unsorted[i,2]
+        unsorted[i], unsorted[i+1] = swap(a,b) if wrong_order?(a,b)
       end
     end
     sorted = unsorted
