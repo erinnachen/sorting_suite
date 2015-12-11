07e4ad256366622188ec3704accfb0cd3d0e1857
Erinna Chen <erinna.chen@gmail.com>
2015-12-10 20:49:11 -0700

    Working inplace insertion sort with tests

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,4 +1,9 @@
 class InsertionSort
+  attr_reader :array
+  def initialize(array = [])
+    @array = array
+  end
+
   def sort(unsorted)
     sorted = []
     unsorted.each do |element|
@@ -11,4 +16,26 @@ class InsertionSort
     sorted
   end
 
+  def inplace_sort
+    sort_ind = 1
+    while sort_ind < array.length
+      to_be_sorted = array[sort_ind]
+      insert_ind = find_insert_ind(sort_ind)
+      sort_ind.downto(insert_ind+1) do |ind|
+        array[ind] = array[ind-1]
+      end
+      array[insert_ind] = to_be_sorted
+      sort_ind += 1
+    end
+    array
+  end
+
+  def find_insert_ind(sort_ind)
+    val = array[sort_ind]
+    insert_ind = sort_ind
+    while insert_ind > 0 && val < array[insert_ind-1]
+      insert_ind -= 1
+    end
+    insert_ind
+  end
 end
--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -7,17 +7,17 @@ class InsertionSortTest < Minitest::Test
     assert_equal [], isort.sort([])
   end
 
-  def test_sort_of_single_element_array_is_the_same_array
+  def test_sort_of_single_element_array_returns_array_with_the_same_value
     isort = InsertionSort.new
     assert_equal ["a"], isort.sort(["a"])
   end
 
-  def test_sorted_two_element_array_returns_the_same_array
+  def test_sorted_two_element_array_returns_an_array_with_elements_still_sorted
     isort = InsertionSort.new
     assert_equal ["a","b"], isort.sort(["a","b"])
   end
 
-  def test_unsorted_two_element_array_returns_the_elements_flipped
+  def test_unsorted_two_element_array_returns_an_array_with_the_elements_flipped
     isort = InsertionSort.new
     assert_equal ["a","b"], isort.sort(["b","a"])
   end
@@ -36,4 +36,62 @@ class InsertionSortTest < Minitest::Test
     assert_equal sorted, isort.sort(shuffled)
   end
 
+end
+
+class InplaceInsertSortTest < Minitest::Test
+  def test_sort_of_empty_array_is_the_same_empty_array
+    array = []
+    ipisort = InsertionSort.new(array)
+    assert_equal [], ipisort.inplace_sort
+    assert_equal array.object_id, ipisort.inplace_sort.object_id
+  end
+
+  def test_sort_of_single_element_array_is_the_same_array
+    array = ["a"]
+    ipisort = InsertionSort.new(array)
+    assert_equal ["a"], ipisort.inplace_sort
+    assert_equal array.object_id, ipisort.inplace_sort.object_id
+  end
+
+  def test_find_insert_ind_finds_the_correct_index_no_swap_needed
+    array = [1,2,3]
+    ipisort = InsertionSort.new(array)
+    assert_equal 2, ipisort.find_insert_ind(2)
+  end
+
+  def test_find_insert_ind_finds_the_correct_insert_index
+    array = [1,2,4,7,9,3]
+    ipisort = InsertionSort.new(array)
+    assert_equal 2, ipisort.find_insert_ind(5)
+  end
+
+  def test_sort_of_sorted_two_element_array_returns_the_same_array
+    array = ["a", "b"]
+    ipisort = InsertionSort.new(array)
+    assert_equal ["a","b"], ipisort.inplace_sort
+    assert_equal array.object_id, ipisort.inplace_sort.object_id
+  end
+
+  def test_sort_of_unsorted_two_element_array_returns_the_same_array_with_elements_sorted
+    array = ["b", "a"]
+    ipisort = InsertionSort.new(array)
+    assert_equal array.object_id, ipisort.inplace_sort.object_id
+    assert_equal ["a","b"], ipisort.inplace_sort
+  end
+
+  def test_sample_sort
+    array = [4,2,0,3,1]
+    ipisort = InsertionSort.new(array)
+    assert_equal array.object_id, ipisort.inplace_sort.object_id
+    assert_equal [0,1,2,3,4], ipisort.inplace_sort
+  end
+
+  def test_sort_a_bunch_of_elements
+    sorted = (1..100).to_a
+    shuffled = sorted.shuffle
+    shuffled = sorted.shuffle while shuffled == sorted
+    ipisort = InsertionSort.new(shuffled)
+    assert_equal shuffled.object_id, ipisort.inplace_sort.object_id
+    assert_equal sorted, ipisort.inplace_sort
+  end
 end
