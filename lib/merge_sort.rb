554f5c7472da4ca01a713c7b907ab85d80539747
Erinna Chen <erinna.chen@gmail.com>
2015-12-11 22:11:46 -0700

    Cleaned up mergesort

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,8 +1,8 @@
 class MergeSort
   def sort(unsorted)
     if unsorted.length > 1
-      nhalf = unsorted.length/2
-      merge(sort(unsorted[0..nhalf-1]), sort(unsorted[nhalf..-1]))
+      ihalf = unsorted.length/2
+      merge(sort(unsorted[0...ihalf]), sort(unsorted[ihalf..-1]))
     else
       unsorted
     end
@@ -10,24 +10,14 @@ class MergeSort
 
   def merge(sorted1,sorted2)
     merged = []
-    i1 = 0
-    i2 = 0
-    while i1 < sorted1.length && i2 < sorted2.length
-      if sorted1[i1] < sorted2[i2]
-        merged << sorted1[i1]
-        i1 += 1
+    until sorted1.empty? || sorted2.empty?
+      if sorted1[0] < sorted2[0]
+        merged << sorted1.shift
       else
-        merged << sorted2[i2]
-        i2 += 1
+        merged << sorted2.shift
       end
     end
-    if i1 < sorted1.length
-      merged+=sorted1[i1..-1]
-    else
-      merged+=sorted2[i2..-1]
-    end
-    merged
+    merged+sorted1+sorted2
   end
-
 
 end
